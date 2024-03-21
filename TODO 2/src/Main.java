d7476eecf678e69c22106dd9a40410ad1aa537fe
Rio <rioloka86@gmail.com>
2024-03-21 18:56:36 +0700

    TODO 2 completed

---
+++ TODO 2/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date waktu = new Date();
+        System.out.println("Tanggal dan waktu saat ini: " + waktu);
+    }
+}
