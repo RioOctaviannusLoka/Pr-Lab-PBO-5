b1b535a44423e90f90e7df4adf3a5cbb56c8fef7
Rio <rioloka86@gmail.com>
2024-03-21 17:54:38 +0700

    TODO 1: instantiation class anjing

--- TODO 1/src/Main.java
+++ TODO 1/src/Main.java
@@ -2,6 +2,30 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Anjing Sony = new Anjing("Sony", "Golden Retriever", 4, "Jantan");
+        Anjing Tifa = new Anjing("Tifa Lockhart", "Chihuahua", 5, "Betina");
+        Anjing Bond = new Anjing("Bond Forger", "Great Pyrenees", 7, "Jantan");
 
+        System.out.println("Anjing 1:");
+        Sony.perkenalan();
+        Sony.mengonggong();
+        Sony.play("Batu");
+        Sony.tidur();
+
+        System.out.println();
+
+        System.out.println("Anjing 2:");
+        Tifa.perkenalan();
+        Tifa.mengonggong();
+        Tifa.play("Cloud Strife");
+        Tifa.tidur();
+
+        System.out.println();
+
+        System.out.println("Anjing 3:");
+        Bond.perkenalan();
+        Bond.mengonggong();
+        Bond.play("Anya Forger");
+        Bond.tidur();
     }
 }
