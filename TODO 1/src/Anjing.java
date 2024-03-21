773737f7c826601beb940e030c9e5a93aaf0fc72
Rio <rioloka86@gmail.com>
2024-03-21 17:52:47 +0700

    TODO 1: membuat method class anjing

--- TODO 1/src/Anjing.java
+++ TODO 1/src/Anjing.java
@@ -10,4 +10,19 @@ public class Anjing {
         this.umur = umur;
         this.gender = gender;
     }
+
+    public void mengonggong() {
+        System.out.println("GUK GUK");
+    }
+    public void perkenalan() {
+        System.out.println("GUK GUK!! Nama saya " + this.nama + ". Saya merupakan seekor anjing jenis " + this.jenis + ". Umur saya " + this.umur + ". Saya berjenis kelamin " + this.gender);
+    }
+    public void tidur() {
+        System.out.println("zzz");
+        System.out.println(this.nama + " sedang tidur. Mohon jangan diganggu.");
+    }
+    public void play(String objek) {
+        System.out.println(this.nama + " sedang bermain dengan "+ objek);
+    }
+
 }
