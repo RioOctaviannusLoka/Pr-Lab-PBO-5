42256f81e8311223ce8e3aaeb89c466ea70faaf5
Rio <rioloka86@gmail.com>
2024-03-21 17:48:14 +0700

    TODO 1: membuat atribut anjing

---
+++ TODO 1/src/Anjing.java
@@ -0,0 +1,13 @@
+public class Anjing {
+    String nama;
+    String jenis;
+    int umur;
+    String gender;
+
+    Anjing(String nama, String jenis, int umur, String gender) {
+        this.nama = nama;
+        this.jenis = jenis;
+        this.umur = umur;
+        this.gender = gender;
+    }
+}
